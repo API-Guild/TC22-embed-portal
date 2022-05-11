26432beb3bdd68b843c938d7aaf84d7ddf988461
Tim Payne <ma7tcsp@gmail.com>
2022-05-11 17:04:48 +0000

    lesson 3_2_1

--- static/js/embed3_2_1.js
+++ static/js/embed3_2_1.js
@@ -26,8 +26,11 @@ function ready() {
           {
             console.log(worksheets[i].name); // Do anything you'd like with a given worksheet
             switch (worksheets[i].name) {
-              // we're working with filters on the sheet called "Sales Map"
-              case 'Sales Map':
+              // Lesson 3_2_1
+              // We're going to be using the filter called Region
+              // And we're working with filters on the sheet called Sales Map
+              // Fill in the name of the sheet in the case statement to set activeFilterSheet
+              case '':
                 // need to save it with variable to use it later in other functions
                 activeFilterSheet = worksheets[i];
                 console.log('Found the sheet I need for filter: ', activeFilterSheet)
@@ -43,11 +46,21 @@ function ready() {
 }
 
 function filterTableau(filterName, value, action = "replace") {
-    activeFilterSheet.applyFilterAsync(filterName, value, action);
+    //Lesson 3_2_1 
+    //call the applyFilterAsync method of the activeFilterSheet
+    //This takes 3 parameters, which are all being passed or set in this fuction
+    //https://help.tableau.com/current/api/embedding_api/en-us/reference/interfaces/worksheet.html#applyfilterasync
+    
+    
 }
 
 function filterClear(filterName) {
-    activeFilterSheet.clearFilterAsync(filterName);
+    //Lesson 3_2_1
+    //similarly to the filterTableau function, but we need to call the clearFilterAync method
+    //the only take a single paramter though 
+    //https://help.tableau.com/current/api/embedding_api/en-us/reference/interfaces/worksheet.html#clearfilterasync
+    
+    
 }
 
 //sheet switching
